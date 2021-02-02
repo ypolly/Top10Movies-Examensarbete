d10b393768e07d9e4f33d7b52abdab1f243688a2
Polina <pollyyaromchyk@gmail.com>
2021-02-02 16:42:45 +0100

    Delete product

--- routes/product.js
+++ routes/product.js
@@ -12,12 +12,29 @@ isAuth,
 isAdmin,
 create
 );
+
+router.delete(
+    "/product/:productId/:userId",
+    requireSignin,
+    isAuth,
+    isAdmin,
+    remove
+);
+
+router.put(
+    "/product/:productId/:userId",
+    requireSignin,
+    isAuth,
+    isAdmin,
+    update
+);
+
 router.get("/product/:productId", read);
 router.get("/products/search", listSearch);
 router.delete("/product/create/:userId", requireSignin, isAuth, isAdmin, remove);
 router.put("/product/:productId/:userId", requireSignin, isAuth, isAdmin, update);
 router.get("/products", list);
-router.get("/products/realted/c", listRelated);
+router.get("/products/related/:productId", listRelated);
 router.get("/products/category", listCategories);
 router.post("/products/by/search", listBySearch);
 router.get("/product/photo/:productId", photo);
